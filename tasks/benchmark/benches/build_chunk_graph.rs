a6267b867e20275ca3d398195a1ba08e94e83bee
Cong-Cong <dacongsama@live.com>
2025-03-14 16:40:35 +0800

    refactor: entries

--- tasks/benchmark/benches/build_chunk_graph.rs
+++ tasks/benchmark/benches/build_chunk_graph.rs
@@ -120,27 +120,24 @@ pub fn build_chunk_graph_benchmark(c: &mut Criterion) {
 
   let compiler_id = compiler.id();
 
-  fast_set(
-    &mut compiler.compilation,
-    Compilation::new(
-      compiler_id,
-      compiler.options.clone(),
-      compiler.plugin_driver.clone(),
-      compiler.buildtime_plugin_driver.clone(),
-      compiler.resolver_factory.clone(),
-      compiler.loader_resolver_factory.clone(),
-      None,
-      compiler.cache.clone(),
-      compiler.old_cache.clone(),
-      Some(Default::default()),
-      Default::default(),
-      Default::default(),
-      compiler.input_filesystem.clone(),
-      compiler.intermediate_filesystem.clone(),
-      compiler.output_filesystem.clone(),
-      false,
-    ),
-  );
+  &mut compiler.compilation = Root::new(Compilation::new(
+    compiler_id,
+    compiler.options.clone(),
+    compiler.plugin_driver.clone(),
+    compiler.buildtime_plugin_driver.clone(),
+    compiler.resolver_factory.clone(),
+    compiler.loader_resolver_factory.clone(),
+    None,
+    compiler.cache.clone(),
+    compiler.old_cache.clone(),
+    Some(Default::default()),
+    Default::default(),
+    Default::default(),
+    compiler.input_filesystem.clone(),
+    compiler.intermediate_filesystem.clone(),
+    compiler.output_filesystem.clone(),
+    false,
+  ));
 
   rt.block_on(async {
     fs.create_dir_all("/src".into())
